4426df48ba18590ed7afd90ddc99690a5988a2ec
callino11 <aishzafra@gmail.com>
2026-03-13 13:50:27 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
-document.getElementById("leadForm")
-.addEventListener("submit", function(e){
+document.getElementById("leadForm").addEventListener("submit", function(e) {
 
-e.preventDefault();
+  e.preventDefault();
 
-const name = document.getElementById("name").value;
-const email = document.getElementById("email").value;
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
 
-fetch("https://script.google.com/macros/s/AKfycbxT271vxwuKKzM8pD9A_J2D0DOPoBJMhB3px_UH4wT3HnDmyPEca7S1GSoTzW4eb6f3CA/exec", {
-method: "POST",
-body: JSON.stringify({
-name: name,
-email: email
-})
-})
-.then(res => res.text())
-.then(data => {
-
-window.location.href = "thankyou.html";
-
-});
+  fetch("https://script.google.com/macros/s/AKfycbxT271vxwuKKzM8pD9A_J2D0DOPoBJMhB3px_UH4wT3HnDmyPEca7S1GSoTzW4eb6f3CA/exec", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email
+    })
+  })
+  .then(response => response.text())
+  .then(data => {
+    console.log(data);
+    window.location.href = "thankyou.html";
+  })
+  .catch(error => {
+    console.error("Error:", error);
+  });
 
 });
