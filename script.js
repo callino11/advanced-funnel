4b4ad580a96c58ea7c0bff0012eeeb76d0549b5a
callino11 <aishzafra@gmail.com>
2026-03-13 13:59:02 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,18 @@
-document.getElementById("leadForm").addEventListener("submit", function(e) {
+document.addEventListener("DOMContentLoaded", function () {
 
-  e.preventDefault();
+const form = document.getElementById("leadForm");
 
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
+form.addEventListener("submit", function(e){
 
-  fetch("https://script.google.com/macros/s/AKfycbxT271vxwuKKzM8pD9A_J2D0DOPoBJMhB3px_UH4wT3HnDmyPEca7S1GSoTzW4eb6f3CA/exec", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      name: name,
-      email: email
-    })
-  })
-  .then(response => response.text())
-  .then(data => {
-    console.log(data);
-    window.location.href = "thankyou.html";
-  })
-  .catch(error => {
-    console.error("Error:", error);
-  });
+e.preventDefault();
 
+const name = document.getElementById("name").value;
+const email = document.getElementById("email").value;
+
+console.log(name,email); // test if button works
+
+// redirect (temporary test)
+window.location.href = "thankyou.html";
+
+});
 });
